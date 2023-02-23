1dea1beb893e2fa7eaa9e5970f9060273259e624
developomp <developomp@gmail.com>
2023-02-23 13:53:16 +0900

    refactor: "docker compose" -> "docker-compose"

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # 이미지를 업데이트하고
-docker compose pull
+docker-compose pull
 
 # docker-compose.prod.yml을 생성하고
-docker compose convert >docker-compose.prod.yml
+docker-compose convert >docker-compose.prod.yml
 
 # docker compose를 background에서 실행
-docker compose --file docker-compose.prod.yml up --detach --build
+docker-compose --file docker-compose.prod.yml up --detach --build
